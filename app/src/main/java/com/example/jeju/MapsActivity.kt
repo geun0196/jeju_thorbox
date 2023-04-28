7c1d3f85d1b2188387d024be07e84ea1fcf22b6c
unknown <geun0196@naver.com>
2023-04-28 21:32:35 +0900

    [FEAT] box2 라벨 생성

--- app/src/main/java/com/example/jeju/MapsActivity.kt
+++ app/src/main/java/com/example/jeju/MapsActivity.kt
@@ -1,22 +1,26 @@
 package com.example.jeju
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.jeju.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.firebase.firestore.FirebaseFirestore
-
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlin.concurrent.thread
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
-    var db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +32,52 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        /////////////////////////////////////////////////////
+        //데이터베이스 접근
+        /////////////////////////////////////////////////////
+        val database: DatabaseReference
+        database = Firebase.database.reference.child("box_number")
+
+        val one_box_database = database.child("1")
+
+        one_box_database.child("weight").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //값이 변경된게 있으면 database의 값이 갱신되면 자동 호출된다.
+                val weight = snapshot.value.toString()
+                binding.weight.setText("$weight kg")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println("Failed to read value.")
+            }
+        })
+
+        val two_box_database = database.child("2")
+
+        two_box_database.child("weight").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //값이 변경된게 있으면 database의 값이 갱신되면 자동 호출된다.
+                val weight = snapshot.value.toString()
+                binding.weight2.setText("$weight kg")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println("Failed to read value.")
+            }
+        })
+
+        /////////////////////////////////////////////////////
+        //버튼 클릭 이벤트
+        /////////////////////////////////////////////////////
+        binding.button.setOnClickListener {
+            val latLng = LatLng(latitude, longitude)
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f))
+        }
+
     }
+
 
     /**
      * Manipulates the map once available.
@@ -39,12 +88,64 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+     var latitude: Double = 0.0
+     var longitude: Double = 0.0
+
+    fun updateGPSData(){
+        val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+        val myRef_latitude : DatabaseReference = database.getReference("gps_latitude")
+
+        myRef_latitude.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //값이 변경된게 있으면 database의 값이 갱신되면 자동 호출된다.
+                var latitude_string = snapshot.value.toString()
+                latitude = latitude_string.toDouble()
+                moveMap(latitude, longitude)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println("Failed to read value.")
+            }
+        })
+
+        val myRef_longitude : DatabaseReference = database.getReference("gps_longitude")
+
+        myRef_longitude.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //값이 변경된게 있으면 database의 값이 갱신되면 자동 호출된다.
+                var longitude_string = snapshot.value.toString()
+                longitude = longitude_string.toDouble()
+                moveMap(latitude, longitude)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println("Failed to read value.")
+            }
+        })
+    }
+
+    var cnt = 0
+    fun moveMap(latitude: Double, longitude: Double){
+        val latLng = LatLng(latitude, longitude)
+
+        mMap.clear()
+        mMap.addMarker(MarkerOptions().position(latLng).title("box"))
+        if(cnt < 3){
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+            cnt++
+        }
+    }
+
+     override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
+
+         thread(start=true){
+             while(true){
+                 Thread.sleep(2000)
+                 updateGPSData()
+             }
+         }
     }
 }
