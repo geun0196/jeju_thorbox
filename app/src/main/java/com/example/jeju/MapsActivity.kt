803c3280cb8ad8b2b91ff1bd0516b378c29b83f7
unknown <geun0196@naver.com>
2023-04-29 09:32:56 +0900

    [REFAC] box를 mutableList로 관리하도록 수정

--- app/src/main/java/com/example/jeju/MapsActivity.kt
+++ app/src/main/java/com/example/jeju/MapsActivity.kt
@@ -13,12 +13,13 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import kotlin.concurrent.thread
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private val boxList = mutableListOf<Box>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,126 +38,84 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val database: DatabaseReference
         database = Firebase.database.reference.child("box_number")
 
-        addValueEventListener(database,"1", binding.weight)
-        addValueEventListener(database,"2", binding.weight2)
+        val box1 = Box(database.child("1"), binding.weight, "box1")
+        boxList.add(box1)
+
+        val box2 = Box(database.child("2"), binding.weight2, "box2")
+        boxList.add(box2)
 
         /////////////////////////////////////////////////////
         //버튼 클릭 이벤트
         //=> 클릭시 일단은 1번 카트로 카메라 움직이게 해놨음. 추후 수정해야함
         /////////////////////////////////////////////////////
         binding.button.setOnClickListener {
-            val latLng = LatLng(latitude1, longitude1)
+            val latLng = LatLng(boxList[0].latitude, boxList[0].longitude)
 
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f))
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,18f))
         }
 
     }
 
-    fun addValueEventListener(database: DatabaseReference, boxNumber: String, textView: TextView) {
-        database.child(boxNumber).child("weight").addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val weight = snapshot.value.toString()
-                textView.text = "$weight kg"
+    inner class Box(databaseRef: DatabaseReference, textView: TextView, title: String) {
+        var title = title
+        var latitude: Double = 0.0
+        var longitude: Double = 0.0
+
+        init {
+            databaseRef.child("weight").addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val weight = snapshot.value.toString()
+                    textView.text = "$weight kg"
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    println("Failed to read value.")
+                }
+            })
+
+            databaseRef.child("gps_latitude").addValueEventListener(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    var latitudeString = snapshot.value.toString()
+                    latitude = latitudeString.toDouble()
+                    updateMap()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    println("Failed to read value.")
+                }
+            })
+
+            databaseRef.child("gps_longitude").addValueEventListener(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    //값이 변경된게 있으면 database의 값이 갱신되면 자동 호출된다.
+                    var longitudeString = snapshot.value.toString()
+                    longitude = longitudeString.toDouble()
+                    updateMap()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    println("Failed to read value.")
+                }
+            })
+        }
+
+        var cnt = 0
+        private fun updateMap() {
+            val latLng = LatLng(latitude, longitude)
+            mMap.clear()
+            for (box in boxList) {
+                mMap.addMarker(MarkerOptions().position(LatLng(box.latitude, box.longitude)).title(box.title))
             }
 
-            override fun onCancelled(error: DatabaseError) {
-                println("Failed to read value.")
+            if(cnt < 2){
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18f))
+                cnt++
             }
-        })
-    }
-
-    var latitude1: Double = 0.0
-    var longitude1: Double = 0.0
-    var latitude2: Double = 0.0
-    var longitude2: Double = 0.0
-
-    fun updateGPSData1(){
-        val database: DatabaseReference
-        database = Firebase.database.reference.child("box_number").child("1")
-
-        database.child("gps_latitude").addValueEventListener(object: ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                //값이 변경된게 있으면 database의 값이 갱신되면 자동 호출된다.
-                var latitude_string = snapshot.value.toString()
-                latitude1 = latitude_string.toDouble()
-                moveMap()
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                println("Failed to read value.")
-            }
-        })
-
-        database.child("gps_longitude").addValueEventListener(object: ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                //값이 변경된게 있으면 database의 값이 갱신되면 자동 호출된다.
-                var longitude_string = snapshot.value.toString()
-                longitude1 = longitude_string.toDouble()
-                moveMap()
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                println("Failed to read value.")
-            }
-        })
-    }
-
-    fun updateGPSData2(){
-        val database: DatabaseReference
-        database = Firebase.database.reference.child("box_number").child("2")
-
-        database.child("gps_latitude").addValueEventListener(object: ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                //값이 변경된게 있으면 database의 값이 갱신되면 자동 호출된다.
-                var latitude_string = snapshot.value.toString()
-                latitude2 = latitude_string.toDouble()
-                moveMap()
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                println("Failed to read value.")
-            }
-        })
-
-        database.child("gps_longitude").addValueEventListener(object: ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                //값이 변경된게 있으면 database의 값이 갱신되면 자동 호출된다.
-                var longitude_string = snapshot.value.toString()
-                longitude2 = longitude_string.toDouble()
-                moveMap()
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                println("Failed to read value.")
-            }
-        })
-    }
-
-    var cnt = 0
-    fun moveMap(){
-        val latLng1 = LatLng(latitude1, longitude1)
-        val latLng2 = LatLng(latitude2, longitude2)
-
-        mMap.clear()
-        mMap.addMarker(MarkerOptions().position(latLng1).title("box1"))
-        mMap.addMarker(MarkerOptions().position(latLng2).title("box2"))
-
-        if(cnt < 3){
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng1, 15f))
-            cnt++
         }
     }
 
      override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap.uiSettings.isZoomControlsEnabled = true
-
-         thread(start=true){
-             while(true){
-                 Thread.sleep(2000)
-                 updateGPSData1()
-                 updateGPSData2()
-             }
-         }
     }
 }
